3c2313700b399dcb34ad4daabbb594b6f7ab3329
David <56525307+Mr-Hansen-Orewa@users.noreply.github.com>
2023-05-23 16:59:41 +1200

    Update SensorPad.ino
    teacher feedback added

--- SensorPad/SensorPad.ino
+++ SensorPad/SensorPad.ino
@@ -1,11 +1,13 @@
 #include <SparkFun_LPS25HB_Arduino_Library.h> 
 #include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 
+//HAN NOTES what are these openlog and lps25hb objects letting us do?
 OpenLog myLog;
 LPS25HB theSensor; 
 
 const byte Openlogadress = 42 //I saw something similiar to this but didn't get to add it and don't remember exactly what it was.
 
+//HAN NOTES add a comment about the method here
 void setup()
 {
   Serial.begin(9600);
@@ -13,15 +15,17 @@ void setup()
   theSensor.begin(); 
   myLog.begin();
 
+  //HAN NOTES what does this if statement do?
   if (theSensor.isConnected() == false) /
   {
     Serial.println("LPS25HB disconnected. Reset the board to try again.");     
   }
-
+//HAN NOTES what is myFile? should it be setup somewhere?
    myLog.append(myFile);
   myLog.println("This goes to the log file");
 }
 
+//HAN NOTES add a comment about the method here
 void loop()
 {
 
