cee304f1f2f09bc1880a91dae35481d891dbf6ba
LunaaLily <124850249+LunaaLily@users.noreply.github.com>
2023-06-21 11:01:20 +1200

    Update SensorPad.ino

--- SensorPad/SensorPad.ino
+++ SensorPad/SensorPad.ino
@@ -1,9 +1,9 @@
-#include <SparkFun_LPS25HB_Arduino_Library.h> 
+#include <SparkFun_LPS25HB_Arduino_Library.h>
 #include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 
 //creates a variable for the OpenLog SD card called "sdCard" and another variable for the LPS25HB temperature sensor called "theSensor".
 OpenLog sdCard;
-LPS25HB theSensor; 
+LPS25HB theSensor;
 
 String myFile = "MaxsFile.txt";
 
@@ -12,17 +12,17 @@ void setup()
 {
   Serial.begin(9600);
   Wire.begin();
-  theSensor.begin(); 
+  theSensor.begin();
   sdCard.begin();
 
-  
+
   //If the sensor is not connected it will present an error message.
-  if (theSensor.isConnected() == false) 
+  if (theSensor.isConnected() == false)
   {
-    Serial.println("The black sensor is disconnected. Reset the board to try again.");     
+    Serial.println("The black sensor is disconnected. Reset the board to try again.");
   }
-//allows the SD card to access my file and sends a message to the file in the SD card.
-   sdCard.append(myFile);
+  //allows the SD card to access my file and sends a message to the file in the SD card.
+  sdCard.append(myFile);
   sdCard.println("Pressure, Temperature");
 }
 
@@ -30,12 +30,12 @@ void setup()
 void loop()
 {
 
-  Serial.print(theSensor.getPressure_hPa()); 
+  Serial.print(theSensor.getPressure_hPa());
   Serial.print("The pressure is () and the temperature is (). ");
-  Serial.println(theSensor.getTemperature_degC()); 
-    sdCard.print(theSensor.getPressure_hPa()); 
-   sdCard.print("The pressure is () and the temperature is (). ");
-  sdCard.println(theSensor.getTemperature_degC()); 
+  Serial.println(theSensor.getTemperature_degC());
+  sdCard.print(theSensor.getPressure_hPa());
+  sdCard.print(", ");
+  sdCard.println(theSensor.getTemperature_degC());
 
   delay(40);
 }
