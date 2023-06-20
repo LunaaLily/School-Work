9cbbef2b5048ea11d62b2b7d8db02aca0c60f2f6
LunaaLily <124850249+LunaaLily@users.noreply.github.com>
2023-06-21 10:56:20 +1200

    Update SensorPad.ino

--- SensorPad/SensorPad.ino
+++ SensorPad/SensorPad.ino
@@ -1,19 +1,19 @@
 #include <SparkFun_LPS25HB_Arduino_Library.h> 
 #include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 
-//HAN NOTES what are these openlog and lps25hb objects letting us do?
-OpenLog myLog;
+//creates a variable for the OpenLog SD card called "sdCard" and another variable for the LPS25HB temperature sensor called "theSensor".
+OpenLog sdCard;
 LPS25HB theSensor; 
 
-const byte Openlogadress = 42 //I saw something similiar to this but didn't get to add it and don't remember exactly what it was.
+String myFile = "MaxsFile.txt";
 
-//HAN NOTES add a comment about the method here
+//Sets up a connection to the arduino board and also sets up a connection from the arduino board to the temperature sesnor and SD card.
 void setup()
 {
   Serial.begin(9600);
   Wire.begin();
   theSensor.begin(); 
-  myLog.begin();
+  sdCard.begin();
 
   
   //If the sensor is not connected it will present an error message.
@@ -21,22 +21,21 @@ void setup()
   {
     Serial.println("The black sensor is disconnected. Reset the board to try again.");     
   }
-//HAN NOTES what is myFile? should it be setup somewhere?
-   myLog.append(myFile);
-  myLog.println("This goes to the log file");
+//allows the SD card to access my file and sends a message to the file in the SD card.
+   sdCard.append(myFile);
+  sdCard.println("Pressure, Temperature");
 }
 
-//HAN NOTES add a comment about the method here
-//Fetches the temperature from the temperature sensor and responds back with the result.
+//Fetches the temperature and pressure from the temperature sensor and responds back with the result on the SD card and serial.
 void loop()
 {
 
   Serial.print(theSensor.getPressure_hPa()); 
-  Serial.print(", ");
+  Serial.print("The pressure is () and the temperature is (). ");
   Serial.println(theSensor.getTemperature_degC()); 
-    myLog.print(theSensor.getPressure_hPa()); 
-   myLog.print(", ");
-  myLog.println(theSensor.getTemperature_degC()); 
+    sdCard.print(theSensor.getPressure_hPa()); 
+   sdCard.print("The pressure is () and the temperature is (). ");
+  sdCard.println(theSensor.getTemperature_degC()); 
 
   delay(40);
 }
